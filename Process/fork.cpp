3883c7233ec47592bc6722be84e19762188b328b
Nguyen Tan Dat <Dat.nguyen_bk@hcmut.edu.vn>
2020-03-31 20:55:12 +0700

    BUG: "wait" was not declared in this scope

---
+++ Process/fork.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid;
+    pid = fork();
+
+    std::cout << "Checking fork()\n";
+    std::cout << pid << '\n';
+
+    if (pid < 0) {
+        fprintf(stderr, "Fork failed\n");
+        return 1;
+    }
+    else if (pid == 0) {
+        execlp("/bin/ls", "ls", NULL);
+    }
+    else {
+        wait(NULL);
+        printf("Child complete\n");
+    }
+    return 0;
+}
